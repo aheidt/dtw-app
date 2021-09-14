426d406394382408e43c19d01c108855baeef2d9
MasterDruid <62264698+aheidt@users.noreply.github.com>
2021-09-15 00:14:28 +0200

    dtw working example

--- main.py
+++ main.py
@@ -209,3 +209,157 @@ save_midi(path=path, pitches=pitches, intervals=intervals, velocities=velocities
 #     except:
 #         menu.render_message(song_path, "Can't play this file", 2000)
 
+# pip install ffmpeg
+# pip install PySoundFile
+
+from __future__ import print_function
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+import librosa
+import librosa.display
+
+# -----------------------------------------------------------------------------
+
+# Visualize
+
+wav_file_true = r'C:\Users\User\Documents\GitHub\DTW\Grieg_Op12_No1_edited_original.wav'
+x_1, fs = librosa.load(wav_file_true)
+plt.figure(figsize=(16, 4))
+librosa.display.waveplot(x_1, sr=fs)
+plt.title('Grieg Op.12 No.1 (True)')
+plt.tight_layout()
+plt.show()
+
+wav_file_false = r'C:\Users\User\Documents\GitHub\DTW\Grieg_Op12_No1_edited_original_false.wav'
+x_2, fs = librosa.load(wav_file_false)
+plt.figure(figsize=(16, 4))
+librosa.display.waveplot(x_2, sr=fs)
+plt.title('Grieg Op.12 No.1 (False)')
+plt.tight_layout()
+plt.show()
+
+# -----------------------------------------------------------------------------
+
+# Extract Chroma Features 
+# List of different features:
+# (https://librosa.org/doc/main/generated/librosa.feature.chroma_stft.html)
+
+n_fft = 4410
+hop_size = 2205
+
+x_1_chroma = librosa.feature.chroma_stft(y=x_1, sr=fs, tuning=0, norm=2,
+                                         hop_length=hop_size, n_fft=n_fft)
+x_2_chroma = librosa.feature.chroma_stft(y=x_2, sr=fs, tuning=0, norm=2,
+                                         hop_length=hop_size, n_fft=n_fft)
+
+plt.figure(figsize=(16, 8))
+plt.subplot(2, 1, 1)
+plt.title('Chroma Representation of $X_1$')
+librosa.display.specshow(x_1_chroma, x_axis='time',
+                         y_axis='chroma', cmap='gray_r', hop_length=hop_size)
+plt.colorbar()
+plt.subplot(2, 1, 2)
+plt.title('Chroma Representation of $X_2$')
+librosa.display.specshow(x_2_chroma, x_axis='time',
+                         y_axis='chroma', cmap='gray_r', hop_length=hop_size)
+plt.colorbar()
+plt.tight_layout()
+plt.show()
+
+# -----------------------------------------------------------------------------
+
+D, wp = librosa.sequence.dtw(X=x_1_chroma, Y=x_2_chroma)
+wp_s = np.asarray(wp) * hop_size / fs
+
+fig = plt.figure(figsize=(10, 10))
+ax = fig.add_subplot(111)
+librosa.display.specshow(D, x_axis='time', y_axis='time',
+                         cmap='gray_r', hop_length=hop_size)
+imax = ax.imshow(D, cmap=plt.get_cmap('gray_r'),
+                 origin='lower', interpolation='nearest', aspect='auto')
+ax.plot(wp_s[:, 1], wp_s[:, 0], marker='o', color='r')
+plt.title('Warping Path on Acc. Cost Matrix $D$')
+plt.colorbar()
+plt.show()
+
+wp_s[0:10]
+wp_s[10:20]
+wp_s[40:50]
+wp_s[140:150]
+wp_s[240:250]
+wp_s[340:350]
+wp_s[450:460]
+wp_s[600:610]
+wp_s[800:810]
+wp_s[900:910]
+wp_s[990:1000]
+
+len(wp_s)
+len(wp_s)/60
+fs
+# -----------------------------------------------------------------------------
+
+fig = plt.figure(figsize=(16, 8))
+
+# Plot x_1
+plt.subplot(2, 1, 1)
+librosa.display.waveplot(x_1, sr=fs)
+plt.title('Slower Version $X_1$')
+ax1 = plt.gca()
+
+# Plot x_2
+plt.subplot(2, 1, 2)
+librosa.display.waveplot(x_2, sr=fs)
+plt.title('Slower Version $X_2$')
+ax2 = plt.gca()
+
+plt.tight_layout()
+
+trans_figure = fig.transFigure.inverted()
+lines = []
+arrows = 30
+points_idx = np.int16(np.round(np.linspace(0, wp.shape[0] - 1, arrows)))
+
+# for tp1, tp2 in zip((wp[points_idx, 0]) * hop_size, (wp[points_idx, 1]) * hop_size):
+for tp1, tp2 in wp[points_idx] * hop_size / fs:
+    # get position on axis for a given index-pair
+    coord1 = trans_figure.transform(ax1.transData.transform([tp1, 0]))
+    coord2 = trans_figure.transform(ax2.transData.transform([tp2, 0]))
+
+    # draw a line
+    line = matplotlib.lines.Line2D((coord1[0], coord2[0]),
+                                   (coord1[1], coord2[1]),
+                                   transform=fig.transFigure,
+                                   color='r')
+    lines.append(line)
+
+fig.lines = lines
+plt.tight_layout()
+plt.show()
+
+# -----------------------------------------------------------------------------
+
+# Example from docs (https://librosa.org/doc/main/generated/librosa.sequence.dtw.html)
+
+import numpy as np
+import matplotlib.pyplot as plt
+y, sr = librosa.load(librosa.ex('brahms'), offset=10, duration=15)
+X = librosa.feature.chroma_cens(y=y, sr=sr)
+noise = np.random.rand(X.shape[0], 200)
+Y = np.concatenate((noise, noise, X, noise), axis=1)
+D, wp = librosa.sequence.dtw(X, Y, subseq=True)
+fig, ax = plt.subplots(nrows=2, sharex=True)
+img = librosa.display.specshow(D, x_axis='frames', y_axis='frames',
+                               ax=ax[0])
+ax[0].set(title='DTW cost', xlabel='Noisy sequence', ylabel='Target')
+ax[0].plot(wp[:, 1], wp[:, 0], label='Optimal path', color='y')
+ax[0].legend()
+fig.colorbar(img, ax=ax[0])
+ax[1].plot(D[-1, :] / wp.shape[0])
+ax[1].set(xlim=[0, Y.shape[1]], ylim=[0, 2],
+          title='Matching cost function')
+plt.show()
+
+# -----------------------------------------------------------------------------
